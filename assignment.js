9833e09571c9be86c72ca92e6556fbaa9ef874e0
Tashrique Ahmed <ta13@williams.edu>
2024-02-07 22:56:09 -0500

    Test commented

--- assignment.js
+++ assignment.js
@@ -124,7 +124,7 @@ function monthlySavings(arr, livingCost) {
 }
 
 
-console.log(monthlySavings([1000, 2000, 3000], 5400));
-console.log(monthlySavings([1000, 2000, 2500], 5000));
-console.log(monthlySavings([900, 2700, 3400], 10000));
-console.log(monthlySavings(100, [900, 2700, 3400]));
+// console.log(monthlySavings([1000, 2000, 3000], 5400));
+// console.log(monthlySavings([1000, 2000, 2500], 5000));
+// console.log(monthlySavings([900, 2700, 3400], 10000));
+// console.log(monthlySavings(100, [900, 2700, 3400]));
